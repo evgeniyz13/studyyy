49b4d6715a3631006670306754c6486fdb04e09f
unknown <evgeniyz1305@gmail.com>
2020-02-05 20:34:10 +0200

    add script file

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 'use strict';
-console.log(leftBorderWidth);
-var leftBorderWidth = 1;//существует всегда
-let second = 2; //создаеться только тогда код дошел к ней
-const pi=3.14;
-var number = 5;
-var string = "Hello!";
-var boolean = true;
+// console.log(leftBorderWidth);
+// var leftBorderWidth = 1;//существует всегда
+// let second = 2; //создаеться только тогда код дошел к ней
+// const pi=3.14;
+// var number = 5;
+// var string = "Hello!";
+// var boolean = true;
 
-var obj = {}
-let persone = {
-    name: "Yevhenii",
-    age: 19
-};//обёект со свойствами имя и возраст
-console.log(persone.age)//вывод нужного свойства
+// var obj = {}
+// let persone = {
+//     name: "Yevhenii",
+//     age: 19
+// };//обёект со свойствами имя и возраст
+// console.log(persone.age)//вывод нужного свойства
 
-let arr = ['qwe.png','orange.jpg','apple.bmp'];// массив
-console.log(arr[1]);//вывод нужного значения массива
+// let arr = ['qwe.png','orange.jpg','apple.bmp'];// массив
+// console.log(arr[1]);//вывод нужного значения массива
 
-alert("nihua");// простое модальное окно
+// alert("nihua");// простое модальное окно
 
-confirm("ty tut?"); // модальное окно с вариантами ответа
+// confirm("ty tut?"); // модальное окно с вариантами ответа
 
-let answer = confirm("ty tut?");//вопрос
-console.log(answer);//вывод ответа
+// let answer = confirm("ty tut?");//вопрос
+// console.log(answer);//вывод ответа
 
-prompt("skolko let?");//модальное окно со строкой ввода
+// prompt("skolko let?");//модальное окно со строкой ввода
 
-let answer2 = prompt("skolko let?", "dohua");//модальное окно со строкой ввода и уже введенным вариантом ответа
-console.log(answer2);
+// let answer2 = prompt("skolko let?", "dohua");//модальное окно со строкой ввода и уже введенным вариантом ответа
+// console.log(answer2);
