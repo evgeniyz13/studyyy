e6488138fae0c3d5d565d54667fb9505b2a844be
Yevhenii <evgeniyz1305@gmail.com>
2021-01-15 19:26:24 +0200

    15.01

--- 1.js
+++ 1.js
@@ -1,19 +1,21 @@
 'use strict';
-const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели', '');
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
-  private: false
+  privat: false
 };
 
-const a = prompt('', ''),
+const a = prompt('Один из последних просмотренных фильмов?', ''),
   b = prompt('На сколько оцените его?', ''),
-  c = prompt('Однин из последних просотреных фильмов?', ''),
+  c = prompt('Один из последних просмотренных фильмов?', ''),
   d = prompt('На сколько оцените его?', '');
+
 personalMovieDB.movies[a] = b;
-personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
 console.log(personalMovieDB);
-jbhjbhbhbjhbjhbjhb
